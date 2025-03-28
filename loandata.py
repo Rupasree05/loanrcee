54637e89c1f211ee4463acb50786cc3d6db91963
Rupasree05 <rupasreesaladi@gmail.com>
2025-03-28 21:14:54 +0530

    Update loandata.py

--- loandata.py
+++ loandata.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 st.title('Loan Approval Process Automation')
-model=joblib.load('loan_data1.joblib')
+model = joblib.load('loan_data1.joblib')
 Gender=st.number_input('enter the gender Male:0 Female:1')
 Married=st.number_input('enter  martial status unmarried:0 married:1')
 Income=st.number_input('Loan ammount in thousands')
